30bd5ee56c55ee176f5bbd678d178af7ff3ff500
@Faimasoft786 <FatimaSoft786@gmail.com>
2024-08-02 10:36:20 +0500

    Recordings updated

--- src/app/[locale]/admin/recording/page.jsx
+++ src/app/[locale]/admin/recording/page.jsx
@@ -5,6 +5,8 @@ import { useTranslations } from 'next-intl';
 const page = () => {
   const  t = useTranslations("Admin")
 const [videos, setVideos] = useState([]);
+
+
   useEffect(() => {
     const fetchVideos = async () => {
       const response = await fetch('https://video-medical-backend-production.up.railway.app/api/recordings/all');
@@ -16,14 +18,14 @@ const [videos, setVideos] = useState([]);
     fetchVideos();
   }, []);
 
-
+// const format1 = format(selectedDate, "MMMM dd yyyy", { locale: localeMap[locale] })
   return (
      <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Registrazioni</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {videos.map((video) => (
           <div key={video._id} className="bg-white rounded-lg shadow p-4">
-            <h2 className="text-xl font-bold mb-2 text-black">{t('Date')}: {moment(video.createdAt).format('DD-MM-YYYY')}</h2>
+            <h2 className="text-xl font-bold mb-2 text-black">{t('Date')}:{video.recordDate}</h2>
             <video 
               className="w-full h-48"
               controls 
